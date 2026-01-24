c328e0983793fd29d8c426c2a3aee23636367e6d
Jeko Paray <jdparay@mcm.edu.ph>
2026-01-24 21:20:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function showSection(sectionId) {
+    const ach = document.getElementById('achievements');
+    const proj = document.getElementById('projects');
+    
+    ach.style.display = (sectionId === 'achievements') ? 'block' : 'none';
+    proj.style.display = (sectionId === 'projects') ? 'block' : 'none';
+    
+    document.getElementById('btn-ach').classList.toggle('active', sectionId === 'achievements');
+    document.getElementById('btn-proj').classList.toggle('active', sectionId === 'projects');
+}
